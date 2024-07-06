a30d3158cfe04e28a71c0b3044deff3dc2780141
空白无上 <hsy040506@163.com>
2024-07-06 19:58:26 +0800

    admin init

---
+++ K-Monitor-Admin/src/main/java/xyz/kbws/MainApplication.java
@@ -0,0 +1,18 @@
+package xyz.kbws;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kbws
+ * @date 2024/7/6
+ * @description: 主启动类
+ */
+@SpringBootApplication
+@Configurable
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ K-Monitor-Admin/src/test/java/xyz/kbws/test/ApiTest.java
@@ -0,0 +1,18 @@
+package xyz.kbws.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author kbws
+ * @date 2024/7/6
+ * @description:
+ */
+@Slf4j
+public class ApiTest {
+
+    @Test
+    public void test_ognl() {
+
+    }
+}
